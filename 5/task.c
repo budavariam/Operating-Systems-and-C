6077f5070c7e5b389f60d0760b1f5a5e27478549
NemesLaszlo <wow.laszlo@gmail.com>
2023-11-14 11:37:56 +0100

    last task

---
+++ 5/task.c
@@ -0,0 +1,31 @@
+#include "sys/types.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "signal.h"
+#include "stdio.h"
+#include "string.h"
+#include "time.h"
+#include "wait.h"
+#include "sys/ipc.h"
+#include "sys/msg.h"
+#include "sys/shm.h"
+#include "sys/sem.h"
+#include "sys/stat.h"
+
+// Sajtótájékoztató kezdődik, ahol a moderátor, (szülő folyamat) a nyilatkozó (gyerek) és a szakértő (gyerek) vesznek részt és válaszolnak a sajtó kérdéseire. 
+// A moderátor irányítja a teljes sajtótájékoztatót és megvárja amig megválaszolásra kerülnek a kérdések. (Szülő bevárja a gyerek folyamat végét, és végig fenntartja 
+// a kapcsolatot)
+// 1, A Sajtótájékoztató kezdetekor a moderátor (szülő) megvárja amig mind két további résztvevő (gyerekek) elfoglalja a helyét, 
+// és jelzést küldenek ( signal ), hogy készen állnak a kezdésre. (Konzolra kiír) Ezt követően a moderátor csövön (pipe) keresztül feltesz egy kérdést a szakértőnek, 
+// megerősítésre várva – „Megvan minden dokumentuma?”. Amire csövön keresztül válaszol, hogy „Igen” (A kommunikáció konzolon kiírásra kerül)
+// 2, Ezt követően a nyilatkozó üzenetsoron keresztül (message queue) hozzászól szintén a kérdéshez, (moderátorhoz) hogy 
+// „Igen méréseink vannak, amiket publikálni fogunk.” A moderátor megjeleníti ezt szintén a konzolon.
+// 3, A nyilatkozó emellett egy mérési számot is megemlít. 
+// (felvett tetszőleges változó) amit osztott memóriába ír. Ahonnan a moderátor kiolvassa és megjeleníti.
+// 4, Védje szemaforral az osztott memória használatát.
+
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
